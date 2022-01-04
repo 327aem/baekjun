719868383962e9f2be6c8faa78d20b3d2806b9c4
Kim Jun Young <81668858+327aem@users.noreply.github.com>
2022-01-04 21:25:03 +0900

    data structure

---
+++ utils.cpp
@@ -0,0 +1,41 @@
+#1. merge_sort
+
+
+void merge(int* v,int left,int middle, int right){
+    if(right-left > n/k) return;
+    int i,i_left,i_right;
+
+    memcpy(buffer+left,v+left,sizeof(int)*(right-left+1));
+
+    i_left = left;
+    i_right = middle+1;
+    i=left;
+
+    while((i_left<=middle) && (i_right<=right)){
+        if(buffer[i_left]<buffer[i_right]){
+            v[i++]=buffer[i_left++];
+        }
+        else{
+            v[i++]=buffer[i_right++];
+        }
+    }
+
+    while(i_left<=middle){
+        v[i++]=buffer[i_left++];
+    }
+    while(i_right<=right){
+        v[i++]=buffer[i_right++];
+    }
+}
+void merge_sort(int* v,int left,int right){
+    int middle;
+
+    if(left<right){
+        middle = (left+right)/2;
+
+        merge_sort(v,left,middle);
+        merge_sort(v,middle+1,right);
+
+        merge(v,left,middle,right);
+    }
+}
